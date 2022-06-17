0b72859ef9495730c1c976885a4aa24bd22f657e
Sanaa3 <94263224+Sanaa3@users.noreply.github.com>
2022-06-17 15:10:02 -0700

    updated video concatenation

--- Library/video_concat.py
+++ Library/video_concat.py
@@ -3,6 +3,7 @@ import array as arr
 import ffmpeg
 from moviepy.editor import *
 from natsort import natsorted
+import numpy as np
 
 #Recursice Function that finds path to mp4 vidoes
 #Stores path in temp_array which is sorted
@@ -16,70 +17,80 @@ def recursiveFind(folder_path, master_array):
 	for path in temp_array: 
 		path = folder_path + '/' + path
 		if ".DS_Store" in path:
+
 			continue
 
 		if ".mp4" in path: #if reaches mp4 videos
 			master_array.append(path)
+			#print(path)
 			#print("STOP")
 			
 		else:
 			recursiveFind(path, master_array) #keeps going inside files
 			#print("KEEP GOING")
 
-	return
+	#print(split_array) #print(split_array[0]) and so on
 
+	return 
 
 
 
 
 
 #concatinating vidoes
-def merge(master_array):
+def merge(split_array, master_array):
 
-	#print(master_array)
+	print(len(master_array))
+	i = 0
 
-	clips = []
+	size = len(master_array)/50
+	print("Size: ", size)
 
-	for paths in master_array:
-		if paths.endswith(".mp4"):
-			#print(paths) 
+	split_array =  np.array_split(master_array, size) 
 
-			filePath = os.paths.join(root, master_array)
-			video = VideoFileClip(filePath)
-			clips.append(video)
-			#print(video)
+	while i < size:
 
-	final_clip = concatenate_videoclips(clips)
-	#final_clip.to_videofile("output.mp4", fps = 24, remove_temp = False)
+		clips = []
 
+		duration = 0
 
-	#printf "file '%s'\n" *.mp4 > list.txt #create list of all mp4s in dc
+		print(len(split_array[i]))
 
-	return
+		for paths in split_array[i]:
+			#print(type(paths))
+			if paths.endswith(".mp4"):
+				print(paths)
 
-"""
-#concatinating vidoes
-def merge(master_array):
+				video = VideoFileClip(paths)
 
-	video_clips = 0
-	final_clip = 0
+				print(video.fps)
 
-	video_arr = []
+				duration = duration + video.duration
+				
+				clips.append(video)
+				
+				#print(video)
+		print(i)
+		#print(duration)
+		i = i + 1
 
-	for path in master_array:
-	   file_path = os.path.join(root, master_array)
-      #video = VideoFileClip(file_path) #make array of this and pass to final clip
+		#print(clips)
 
-   	video_arr.append(VideoFileClip(file_path))
+		final_clip = concatenate_videoclips(clips)
 
-	final_clip = concatenate_videoclips(video_arr)
-	final_clip.to_videofile("output.mp4", fps=24, remove_temp=False)
+		#final_clip = (final_clip.subclip(t_end=round(final_clip.duration - 1.0/final_clip.fps), 2))
 
-	print(final_clip)
+		final_clip.duration = round(final_clip.duration, 2)
 
-	return
+		#if final_clip != 0:
+		#print(final_clip.duration)
+		final_clip.write_videofile("output.mp4", fps=15.0, remove_temp=False)
 
-"""
+		for video in clips:
+			video.close()
+
+	return split_array
+
 
 
 
@@ -87,14 +98,18 @@ def merge(master_array):
 #Main function that calls recursive function
 def main():
 	print('Current working directory : ', os.getcwd())
-	folder1_path = os.getcwd() + '/dates'
+	folder1_path = os.getcwd() + '/data/dates'
 	print(os.listdir(os.getcwd()))
 
 	master_array = [] #fill array with contents of directory
 
-	recursiveFind(folder1_path, master_array)
+	split_array = recursiveFind(folder1_path, master_array)
 
-	merge(master_array)
+	#recursiveFind(folder1_path, master_array)
+
+	#print(split_array)
+
+	merge(split_array, master_array)
 
 	#print(master_array)
 
